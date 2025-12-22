ecb98ed2ff3e6872bdd69fad3cbec19a690fcb15
shivasayz <writeforshivaa@gmail.com>
2025-12-22 18:48:49 +0530

    db connected successful

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config({ path: './config.env' });
 
@@ -5,7 +6,19 @@ import express from 'express';
 import morgan from 'morgan';
 import app from './app.js';
 
+
 // console.log(process.env.NODE_ENV); // now it will print "development"
+const db = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+console.log(db);
+
+mongoose.connect(db, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+}).then(con => {
+  console.log(con.connection);
+  console.log("DB connected successful")
+});
 
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
