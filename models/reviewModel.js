dac07b2f65c4ad7b3a6e495ec90457ab6d78edfe
shivasayz <writeforshivaa@gmail.com>
2026-02-07 17:34:53 +0530

    cal avg stats: 2

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -67,15 +67,31 @@ reviewSchema.statics.calAverageRating = async function (tourId) {
     },
   ]);
 
-  await Tour.findByIdAndUpdate(tourId, {
-    ratingQuantity: stats[0].nRating,
-    ratingAverage: stats[0].avgRating
-  })
+  if (stats.length > 0) {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: stats[0].nRating,
+      ratingAverage: stats[0].avgRating,
+    });
+  } else {
+    await Tour.findByIdAndUpdate(tourId, {
+      ratingQuantity: 0,
+      ratingAverage: 4.5,
+    });
+  }
 };
 
 reviewSchema.post('save', function(){
   this.constructor.calAverageRating(this.tour);
 })
+
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  await this.r.constructor.calAverageRating(this.r.tour);
+});
 
 const Review = mongoose.model('Review', reviewSchema);
 export default Review;
