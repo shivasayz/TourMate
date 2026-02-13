38323b98a119ff3f69b11efd06672c693b522c3f
shivasayz <writeforshivaa@gmail.com>
2026-02-13 17:01:26 +0530

    updating user data thru API

---
+++ public/js/index.js
@@ -0,0 +1,26 @@
+import '@babel/polyfill';
+import { login, logout } from './login';
+import { updateData } from './updateSettings';
+
+const form = document.querySelector('.form--login');
+const logOutBtn = document.querySelector('.nav__el--logout');
+const userDataForm = document.querySelector('.form-user-data');
+
+if (form) {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    login(email, password);
+  });
+}
+
+if (logOutBtn) logOutBtn.addEventListener('click', logout);
+if (userDataForm)
+  userDataForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const email = document.getElementById('email').value;
+    const name = document.getElementById('name').value;
+
+    updateData(name, email);
+  });
---
+++ public/js/login.js
@@ -0,0 +1,43 @@
+import axios from 'axios';
+import { showAlert, hideAlert } from './alerts';
+
+export const login = async (email, password) => {
+  try {
+    const response = await axios({
+      method: 'POST',
+      url: '/api/v1/users/login',
+      data: {
+        email,
+        password,
+      },
+    });
+
+    if (response.data.status === 'success') {
+      showAlert('success', 'Logged in successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    const msg =
+      err.response?.data?.message || // if your backend uses top-level message
+      err.response?.data?.error?.message || // if your backend nests message
+      'Something went wrong';
+
+    showAlert('error', msg);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: '/api/v1/users/logout',
+    });
+
+    if (res.data.status === 'success') location.reload(true);
+  } catch (err) {
+    console.log(err.response);
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
