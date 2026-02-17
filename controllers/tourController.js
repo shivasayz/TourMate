4955353cb200087117e9d98f3eb5fb0e56280bda
shivasayz <writeforshivaa@gmail.com>
2026-02-17 17:16:44 +0530

    processing multiple images

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -7,7 +7,7 @@ import {
   createOne,
   getOne,
   getAll,
-} from '../controllers/handlerFactory.js';
+} from './handlerFactory.js';
 import appError from '../utils/appError.js';
 import multer from 'multer';
 import sharp from 'sharp';
@@ -45,10 +45,36 @@ const uploadTourImages = upload.fields([
   },
 ]);
 
-const resizeImages = (req, res, next) => {
-  console.log(req.files);
+const resizeImages = catchAsync(async (req, res, next) => {
+  if (!req.files.imageCover || !req.files.images) return next();
+
+  // 1) Cover image
+  req.body.imageCover = `tour-${req.params.id}-${Date.now()}-cover.jpeg`;
+  await sharp(req.files.imageCover[0].buffer)
+    .resize(2000, 1333)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/tours/${req.body.imageCover}`);
+
+  // 2) Images
+  req.body.images = [];
+
+  await Promise.all(
+    req.files.images.map(async (file, i) => {
+      const filename = `tour-${req.params.id}-${Date.now()}-${i + 1}.jpeg`;
+
+      await sharp(file.buffer)
+        .resize(2000, 1333)
+        .toFormat('jpeg')
+        .jpeg({ quality: 90 })
+        .toFile(`public/img/tours/${filename}`);
+
+      req.body.images.push(filename);
+    }),
+  );
+
   next();
-};
+});
 
 // middlewares
 const aliasTopTours = (req, res, next) => {
