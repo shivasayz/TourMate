9c8184751aeccbae08fd21da429d47020e065233
shivasayz <writeforshivaa@gmail.com>
2026-02-20 19:13:35 +0530

    Booking model added

---
+++ models/bookingModel.js
@@ -0,0 +1,42 @@
+import { mongo, Mongoose } from "mongoose";
+import * as mongoose from 'mongoose';
+
+const bookingSchema = new Mongoose.Schema({
+  tour:  {
+    type: mongoose.Schema.ObjectId,
+    ref: 'Tour',
+    required: [true, 'Booking must belong to a Tour.']
+  },
+
+  user: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+    required: [true, 'Booking must belong to a User.']
+  },
+
+  price: {
+    type: Number,
+    required: [true, 'Booking must have a price.']
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now()
+  },
+
+  paid: {
+    type: Boolean,
+    default: true
+  }
+});
+
+bookingSchema.pre(/^find/, function(next) {
+  this.populate('user').populate({
+    path: 'tour',
+    select: 'name'
+  });
+  next();
+});
+
+const Booking = mongoose.model('Booking', bookingSchema);
+export default Booking;
--- models/modelsExport.js
+++ models/modelsExport.js
@@ -1,9 +1,11 @@
 import Tour from "./tourModels.js";
 import User from "./userModel.js";
 import Review from "./reviewModel.js"
+import Booking from "./bookingModel.js"
 
 export  {
     Tour,
     User,
-    Review
+    Review,
+    Booking
 }
